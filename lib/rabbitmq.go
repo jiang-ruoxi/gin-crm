3e1c51a8ff1141bff4566b7aa7c7dbfaaaabf166
jiang-ruoxi <jiangzunshao@163.com>
2022-04-22 18:51:18 +0800

    Update rabbitmq.go

--- lib/rabbitmq.go
+++ lib/rabbitmq.go
@@ -63,6 +63,79 @@ func (r *RabbitMQ) RabbitMQClose() {
 	_ = r.Channel.Close()
 }
 
+//PublishData 数据内容写入生成者
+func (r *RabbitMQ) PublishData(message string) error {
+	//发送消息到队列中
+	//Publish(交换器名称, 队列名称, 是否为无法路由的消息进行返回处理, 是否对路由到无消费者队列的消息进行返回处理, 消息体)
+	if err := r.Channel.Publish(r.Exchange, r.Queue, false, false, amqp.Publishing{
+		Headers:     amqp.Table{},
+		Timestamp:   time.Now(),
+		ContentType: "text/plain",
+		Body:        []byte(message),
+	}); err != nil {
+		log.Printf("[amqp] publish message error: %s\n", err)
+		return err
+	}
+
+	fmt.Println(time.Now().Format("2006-01-02 15:04:05") + " [result]:: message send successful")
+	return nil
+}
+
+//DeclareExchange 创建或者连接一个交换器
+func (r *RabbitMQ) DeclareExchange(kind string) error {
+	//ExchangeDeclare(交换机名称, 交换机类型, 是否持久化, 是否自动删除, 是否是内置交换机, 是否等待服务器确认,args Table)
+	if err := r.Channel.ExchangeDeclare(r.Exchange, kind, true, false, false, true, nil); err != nil {
+		log.Printf("[amqp] declare exchange error: %s\n", err)
+		return err
+	}
+	return nil
+}
+
+//DeclareQueue 创建或者连接一个队列
+func (r *RabbitMQ) DeclareQueue() error {
+	//QueueDeclare(队列名称, 是否持久化, 是否自动删除, 是否排他, 是否等待服务器确认, args Table)
+	_, err := r.Channel.QueueDeclare(r.Queue, false, false, false, false, nil)
+	if err != nil {
+		log.Printf("[amqp] declare queue error: %s\n", err)
+		return err
+	}
+	return nil
+}
+
+//BindQueue 绑定交换器和队列
+func (r *RabbitMQ) BindQueue(key string, nowait bool) error {
+	// QueueBind(队列名称, 根据交换机类型来设定, 交换机名称 string, 是否等待服务器确认, args Table)
+	if err := r.Channel.QueueBind(r.Queue, key, r.Exchange, nowait, nil); err != nil {
+		log.Printf("[amqp] bind queue error: %s\n", err)
+		return err
+	}
+	return nil
+}
+
+func (r *RabbitMQ) ConsumeQueue() error {
+	// 接收消息
+	msgChan, err := r.Channel.Consume(r.Queue, "", true, false, false, false, nil)
+	if err != nil {
+		log.Printf("[amqp] consume queue error: %s\n", err)
+		return err
+	}
+
+	forever := make(chan bool)
+	//go 协程方式处理数据
+	go func() {
+		for d := range msgChan {
+			//log.Printf是以控制台输出的方式进行打印展示
+			log.Printf("Received a message: %s", d.Body)
+
+			//这里需要根据业务的需求处理对应的业务逻辑
+			DealConsumeData(d.Body)
+		}
+	}()
+	log.Printf("[*] Waiting for message, To exit press CTRL+C")
+	<-forever
+	return nil
+}
+
 //NewRabbitMQSimple 创建简单模式实例对象
 func NewRabbitMQSimple(queue string) *RabbitMQ {
 	return CreateRabbitMQ(queue, "", "")
@@ -78,30 +151,16 @@ func (r *RabbitMQ) SimplePublishData(message string) {
 	defer r.RabbitMQClose()
 	//前两步骤已在创建对象连接的时候已经实现，现在需要从第三步骤开始即可
 	//创建或者连接一个交换器
-	//ExchangeDeclare(交换机名称, 交换机类型, 是否持久化, 是否自动删除, 是否是内置交换机, 是否等待服务器确认,args Table)
-	//err := r.Channel.ExchangeDeclare(r.Exchange, "direct", true, false, false, true, nil)
-	//r.ErrFailInfoLog(err, "创建或者连接一个交换器--失败！")
+	//_ = r.DeclareExchange("direct")
 
 	//创建或者连接一个队列
-	//QueueDeclare(队列名称, 是否持久化, 是否自动删除, 是否排他, 是否等待服务器确认, args Table)
-	_, err := r.Channel.QueueDeclare(r.Queue, false, false, false, false, nil)
-	r.ErrFailInfoLog(err, r.Queue+"创建或者连接一个队列--失败！")
+	_ = r.DeclareQueue()
 
 	//绑定交换器和队列
-	// QueueBind(队列名称, 根据交换机类型来设定, 交换机名称 string, 是否等待服务器确认, args Table)
-	//err = r.Channel.QueueBind(r.Queue, "direct", r.Exchange, true, nil)
-	//r.ErrFailInfoLog(err, "绑定交换器和队列--失败！")
+	//_=r.BindQueue("direct", true)
 
 	//发送消息到队列中
-	//Publish(交换器名称, 队列名称, 是否为无法路由的消息进行返回处理, 是否对路由到无消费者队列的消息进行返回处理, 消息体)
-	err = r.Channel.Publish(r.Exchange, r.Queue, false, false, amqp.Publishing{
-		Timestamp:   time.Now(),
-		ContentType: "text/plain",
-		Body:        []byte(message),
-	})
-	if err != nil {
-		fmt.Printf("消息::%v 发送--失败\n", message)
-	}
+	_ = r.PublishData(message)
 }
 
 //SimpleConsumeData 简单模式消费者
@@ -110,26 +169,10 @@ func (r *RabbitMQ) SimpleConsumeData() {
 	// 接收消息
 	// Consume(队列名称, 消费者, 是否自动确认, 是否具有排他性, noLocal, 队列消费是否阻塞, 其他配置)
 	// 保证队列存在
-	_, err := r.Channel.QueueDeclare(r.Queue, false, false, false, false, nil)
-	r.ErrFailInfoLog(err, r.Queue+"创建或者连接一个队列--失败！")
+	_ = r.DeclareQueue()
 
 	// 接收消息
-	msgChan, err := r.Channel.Consume(r.Queue, "", true, false, false, false, nil)
-	r.ErrFailInfoLog(err, "消息接收--失败！")
-
-	forever := make(chan bool)
-	//go 协程方式处理数据
-	go func() {
-		for d := range msgChan {
-			//log.Printf是以控制台输出的方式进行打印展示
-			log.Printf("Received a message: %s", d.Body)
-
-			//这里需要根据业务的需求处理对应的业务逻辑
-			DealConsumeData(d.Body)
-		}
-	}()
-	log.Printf("[*] Waiting for message, To exit press CTRL+C")
-	<-forever
+	_ = r.ConsumeQueue()
 }
 
 //People e.g. 案例示例
